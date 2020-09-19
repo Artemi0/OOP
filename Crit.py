9f551a542660b8d11788955fef69c3b3e4b83121
Artemi0 <artem1012.aa@gmail.com>
2020-09-19 18:10:20 +0300

    final

--- Crit.py
+++ Crit.py
@@ -1,36 +1,107 @@
 class Critter:
-    total = 0 
+    """Виртуальный питомец"""
+    total = 0
 
-    @staticmethod
+    @staticmethod   
     def status():
         print("Общее число зверюшек", Critter.total)
 
-    """Виртуальный питомец """
-    def __init__(self, name, hunger = 0 , boredom = 0):
-        self.name = name
-        self.hunger = hunger 
-        self.boredom = boredom 
+    def __init__(self, name, hunger = 0, boredom = 0):
+        self.__name = name
+        self.hunger = hunger
+        self.boredom = boredom
         Critter.total += 1
-    
-    def talk(self):
-        print("My name is ", self.name)
 
     def __str__(self):
-        ans = "Объект класса Critter\n"
-        ans += "имя: " + self.name + "\n"
-        return ans    
+        ans = 'Объект класса Critter\n'
+        ans += 'имя: ' + self.name + '\n'
+        return ans
+    
+    def __pass_time(self):
+        self.hunger += 1
+        self.boredom += 1
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if new_name == "":
+            print("Имя зверушки не может быть пустой строкой.")
+        else:
+            self.__name = new_name
+            print("Имя успешно изменено.")
+
+    @property
+    def mood(self):
+        unhappiness = self.hunger + self.boredom
+        if unhappiness < 5:
+            m = "прекрасно"
+        elif 5 <= unhappiness <= 10:
+            m = "неплохо"
+        elif 11 <= unhappiness <= 15:
+            m = "не сказать чтобы хорошо"
+        else:
+            m = "ужасно"
+        return m
+
+    def talk(self):
+        print("Меня зовут", self.name, 
+              ", и сейчас я чувствую себя", self.mood)
+        self.__pass_time()
+
+    def eat(self, food = 4):
+        print("Мррр...  Спасибо!")
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        self.__pass_time()
+
+    def play(self, fun = 4):
+        print("Уиии!")
+        self.boredom -= fun
+        if self.boredom < 0:
+            self.boredom = 0
+        self.__pass_time()
 
 def main():
-    print("Доступ к атрибуту класса Critter.total: ",end = " " )
-    print(Critter.total)
+    crit_name = input("Как вы назовете свою зверюшку?: ")
+    crit = Critter(crit_name)
 
-    print("Создание зверюшек. ")
-    crit1 = Critter("Зверушка 1 ")
-    crit2 = Critter("Зверушка 2 ")
-    crit3 = Critter("Зверушка 3 ")
+    choice = None  
+    while choice != "0":
+        print \
+        ("""
+        Моя зверюшка
     
-    Critter.status()
+        0 - Выйти
+        1 - Узнать о самочувствии зверюшки
+        2 - Покормить зверюшку
+        3 - Поиграть со зверюшкой
+        """)
+    
+        choice = input("Ваш выбор: ")
+        print()
 
-    print("Доступ к атрибуту класса через объект: ", end=" ")
-    print(crit1.total)
-main()            
+        # выход
+        if choice == "0":
+            print("До свидания.")
+
+        # беседа со зверюшкой
+        elif choice == "1":
+            crit.talk()
+        
+        # кормление зверюшки
+        elif choice == "2":
+            crit.eat()
+         
+        # игра со зверюшкой
+        elif choice == "3":
+            crit.play()
+
+        # непонятный пользовательский ввод
+        else:
+            print("Извините, в меню нет пункта", choice)
+    
+main()       
